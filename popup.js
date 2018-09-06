28b4f6c70eb27819345531c6fb7d4f7a1c35442f
Dan Molina <digzmol45@gmail.com>
2018-09-05 22:05:11 -0700

    first update, not fully functional yet

--- popup.js
+++ popup.js
@@ -1,21 +1,24 @@
 // version 1 only provides support for the current price of four currencies 
 // to be added: select specific currencies and display previous days price 
-
+console.log("hello");
 
 var symbols = ['BTC', 'ETH', 'BCH' , 'LTC'];
-
+var ccEndpoint = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=";
 
 var coinRequest1 = new XMLHttpRequest();
+var i = 0;
 
- for (i in symbols)
- {
 
- coinRequest1.open("GET", "https://min-api.cryptocompare.com/data/price?fsym=" + symbols[i] + "&tsyms=USD&e=Coinbase",false);
- coinRequest1.send(null);
+// for ( i = 0 , i < symbols.len(), i++)
+ //{
+ 	//ccEndpoint += symbol + ",";
+
+ //}
+
+console.log(ccEndpoint);
+// coinRequest1.open("GET", "https://min-api.cryptocompare.com/data/price?fsym=" + symbols[i] + "&tsyms=USD&e=Coinbase",false);
+ //coinRequest1.send(null);
  
- var currentPrice = JSON.parse(coinRequest1.response).USD;
- var currentDisplay = document.getElementById("coin" + i);
- currentDisplay.innerHTML = symbols[i] + " " + currentPrice;
-
- }
-
+// var currentPrice = JSON.parse(coinRequest1.response).USD;
+ //var currentDisplay = document.getElementById("coin" + i);
+ //currentDisplay.innerHTML = symbols[i] + " " + currentPrice;
